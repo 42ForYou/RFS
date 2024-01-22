665f0dc7599df5fbf4f0c0fad559f1105a1330ad
daekuelee <daekuelee@gmail.com>
2024-01-23 03:01:40 +0900

    FY-89/refactor(core): 재구현된 createElement함수 와 관련된 테스트 추가
    createElement 함수의 초기 구현에 문제가 발견되어 이를 재구현 햇습니다. 이 과정에서 올바른 구현과 현재 동작 방식을 명확히 비교하기 위해 createElement.test.html 파일을 추가했습니다. 이 파일은 모듈 기반의 테스트가 어려움을 극복하기 위한 것으로, createElement 함수의 새로운 구현이 기대한 대로 정확하게 동작하는지 검증하기 위해 사용되었습니다.
    
    close FY-89

--- srcs/core/createElement.js
+++ srcs/core/createElement.js
@@ -1,25 +1,30 @@
-import { isArray as isArr } from '../shared/isArray';
+import { isArray as isArr } from "../shared/isArray";
 
 const RfsElement = (type, props, children) => {
     return {
-    type: type === Fragment ? 'fragment' : type,
-    key: props?.key,
-    ref: props?.ref,
-    props: {
-      ...props,
-      children
-    }
+        type: type === Fragment ? "fragment" : type,
+        key: props?.key,
+        ref: props?.ref,
+        props: {
+            ...props,
+            children,
+        },
     };
-}  
+};
+
+const flatten = (arr) => {
+    return arr.reduce((acc, val) => {
+        if (val === null || val === undefined) {
+            return acc;
+        }
+        return isArray(val) ? acc.concat(flatten(val)) : acc.concat(val);
+    }, []);
+};
 
 export const createElement = (type, props = {}, ...children) => {
-    return RfsElement(type, props, children.flat(Infinity));
-}
-  
+    return RfsElement(type, props, flatten(children));
+};
+
 export const Fragment = (props) => {
     return props.children;
-  }
-  
-
-  
-  
+};
