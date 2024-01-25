c81e2c86f65f6f218cba3303a1a83636ea1e74fa
JinMu Go <jingmuio@gmail.com>
2024-01-25 20:29:26 +0900

    FY-64/chore(dispatcher): add hookDispatcher
    현재 component의 상태에 따른 dispatcher를 정의합니다. dispatcher는
    이후 추가될 renderWithHooks에 의해 mount, update가 결정 됩니다.

---
+++ srcs/hooks/core/hooksDispatcher.js
@@ -0,0 +1,32 @@
+/**
+ * @module hooksDispatcher
+ * @description This module defines the hooksDispatcher object.
+ */
+
+/**
+ * @description This object is dispatcher for mount hooks.
+ * @property {Function} useState
+ * @property {Function} useReducer
+ * @property {Function} useEffect
+ * @property {Function} useMemo
+ */
+export const HookDispatcherOnMount = {
+    // useState: mountState,
+    // useReducer: mountReducer,
+    // useEffect: mountEffect,
+    // useMemo: mountMemo,
+};
+
+/**
+ * @decription This object is dispatcher for update hooks.
+ * @property {Function} useState
+ * @property {Function} useReducer
+ * @property {Function} useEffect
+ * @property {Function} useMemo
+ */
+export const HookDispatcherOnUpdate = {
+    // useState: updateState,
+    // useReducer: updateReducer,
+    // useEffect: updateEffect,
+    // useMemo: updateMemo,
+};
