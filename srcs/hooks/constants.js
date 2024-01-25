bf8c2d4544bb7bd215e91fc4ce948df99d39486a
JinMu Go <jingmuio@gmail.com>
2024-01-25 18:53:07 +0900

    FY-64/chore: modify constants
    type을 정의하기 위해 있었던 객체 defaultHook 과 effectValue를 types.js로
    옮겼습니다.

--- srcs/hooks/constants.js
+++ srcs/hooks/constants.js
@@ -8,31 +8,5 @@
  * @description Symbol for the useEffect tag.
  */
 export const ONCE = Symbol.for("ONCE");
-export const EXEC_EFFECT = Symbol.for("USEEFFECT");
-export const NO_CHANGES = Symbol.for("NOCHANGES");
-
-/**
- * @property {Object | Value} memoizedState
- * @property {Object} queue
- * @property {Hook} next
- */
-export const defaultHook = Object.freeze({
-    memoizedState: null,
-    queue: null,
-    next: null,
-});
-
-/**
- * @property {Symbol} tag
- * @property {Function} create
- * @property {Function} destroy
- * @property {Array} deps
- * @property {effectValue} next
- */
-export const effectValue = Object.freeze({
-    tag: null,
-    create: null,
-    destroy: null,
-    deps: null,
-    next: null,
-});
+export const EXEC_EFFECT = Symbol.for("EXEC_EFFECT");
+export const NO_CHANGES = Symbol.for("NO_CHANGES");
