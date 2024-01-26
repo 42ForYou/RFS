d8ec5c1aac047c8d082402db704b5fb3c3ad8e24
JinMu Go <jingmuio@gmail.com>
2024-01-26 20:37:09 +0900

    FY-67/docs(types): add types for useReducer
    useReducer를 구현하기 위한 type인 TUpdate,TUpdateQueue를 정의하였습니다.

--- types.js
+++ types.js
@@ -9,6 +9,43 @@
 // const NO_CHANGES = Symbol.for("NOCHANGES");
 
 /**
+ * @property {any} action
+ * @property {boolean} hasEagerState
+ * @property {any} eagerState
+ * @property {TUpdate | null} next
+ */
+const TUpdate = Object.freeze({
+    action: null,
+    // for optimization of useReducer not now
+    /*
+        if (update.hasEagerState) {
+        // If this update is a state update (not a reducer) and was processed eagerly,
+        // we can use the eagerly computed state
+        newState = ((update.eagerState: any): S);
+        } else {
+          newState = reducer(newState, action);
+        }
+    */
+    // hasEagerState: null,
+    // eagerState: null,
+
+    next: null,
+});
+
+/**
+ * @property {TUpdate} pending
+ * @property {function} dispatch
+ * @property {function | baseicStateReducer} lastRenderedReducer
+ * @property {any} lastRenderedState
+ */
+const TUpdateQueue = Object.freeze({
+    pending: null,
+    dispatch: null,
+    lastRenderedReducer: null,
+    lastRenderedState: null,
+});
+
+/**
  * @property {Symbol} tag
  * @property {Function} create
  * @property {Function} destroy
