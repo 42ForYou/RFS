870b3af4ca2eba3a2dcffddcabe88158427eeeee
JinMu Go <jingmuio@gmail.com>
2024-02-16 14:54:32 +0900

    FY-64/fix: renderWithHooks
    finishRenderingHook함수를 추가하였고 renderWithHooks는 컴포넌트를 새로
    렌더링하는 함수이끼 때무네 WIP fiber의 hook list를 관리하는
    memoizedState와 Effect 객체를 관리하는 updateQueue를 초기화합니다.

--- srcs/hooks/core/renderWithHooks.js
+++ srcs/hooks/core/renderWithHooks.js
@@ -3,6 +3,20 @@ import {
     hookDispatcherOnMount,
     hookDispatcherOnUpdate,
 } from "./hooksDispatcher";
+
+/**
+ *
+ * @param {Ojbect} hookCore
+ * @description This function is used to finish rendering hooks.
+ * 하나의 컴포넌트의 렌더링이 끝났기 때문에 hookCore에 존재하던 property들을 초기화합니다.
+ * @see hookCore
+ */
+const finishRenderingHooks = (hookCore) => {
+    hookCore.currentlyRenderingFiber = null;
+    hookCore.workInProgressHook = null;
+    hookCore.currentHook = null;
+};
+
 /**
  * @function renderWithHooks
  * @description This function is used to render the component with hooks.
@@ -22,6 +36,9 @@ export default renderWithHooks = (
 ) => {
     hookCore.currentlyRenderingFiber = workInProgress;
 
+    workInProgress.memoizedState = null;
+    workInProgress.updateQueue = null;
+
     if (current === null || current.memoizedState === null) {
         hookCore.RfsCurrentDispatcher.current = hookDispatcherOnMount;
     } else {
@@ -29,8 +46,6 @@ export default renderWithHooks = (
     }
 
     const children = Component(props, secondArg);
-    hookCore.currentlyRenderingFiber = null;
-    hookCore.workInProgressHook = null;
-    hookCore.currentHook = null;
+    finishRenderingHooks(hookCore);
     return children;
 };
