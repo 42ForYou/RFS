eb33cd8592e9c2fe7b32269afd6705279c51af1c
JinMu Go <jingmuio@gmail.com>
2024-02-19 15:37:30 +0900

    FY-137/docs(effectInstance): add Missing Object into useEffect

---
+++ srcs/hooks/types/TEffectInstance.js
@@ -0,0 +1,12 @@
+/**
+ * @typedef {Object} TEffectInstance
+ * @property {Function} destroy - cleanup function
+ * @description - Effect Instance
+ * destroy function은 effect객체와 사용되는 시점이 다를 수 있습니다.
+ * 다시말하면, effect객체가 소멸되었어도 destroy함수를 사용할 수 있는데
+ * 이 때를 위한 객체가 바로 EffectInstance입니다.
+ * 추후 그 문맥과 시점을 정확히 할 예정입니다.
+ */
+const TEffectInstance = {
+    destroy: undefined | Function,
+};
