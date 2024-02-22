0f54d22d278513b30314ae37705bf65709eb9fce
JinMu Go <jingmuio@gmail.com>
2024-02-23 08:55:15 +0900

    FY-185/feat(hooks): add useMemo (#17)

--- srcs/hooks/index.js
+++ srcs/hooks/index.js
@@ -4,5 +4,6 @@ import useLayoutEffect from "./useLayoutEffect/useLayoutEffect.js";
 import useState from "./useState/useState.js";
 import useCallback from "./useCallback/useCallback.js";
 import useRef from "./useRef/useRef.js";
+import useMemo from "./useMemo/useMemo.js";
 
-export { useReducer, useEffect, useCallback, useRef, useState, useLayoutEffect };
+export { useReducer, useEffect, useCallback, useRef, useState, useLayoutEffect, useMemo };
