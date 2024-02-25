9bb36d0248e9c3e19d9249875e78de7d1da095c5
JinMu Go <jingmuio@gmail.com>
2024-02-25 18:37:18 +0900

    FY-193/feat(workInProess): renew to 16.12.0

--- srcs/hooks/core/workInProgressHook.js
+++ srcs/hooks/core/workInProgressHook.js
@@ -14,12 +14,13 @@ import { createHookObject } from "../constructor/index.js";
  * @see workInProgressHook
  * @description This function pushes back the hook list.
  * update와 mount의 공통적인 로직을 추상화하여 하나의 함수로 만들었습니다.
+ * firstWorkInProgressHook는 말 그대로 첫번째 workInProgressHook을 가리킵니다.
  * 주요 로직은 아래 주석을 참고하시기 바랍니다.
  */
 const pushBackHookList = (newHook) => {
     if (hookCore.workInProgressHook === null) {
         // This is the first hook in the list.
-        hookCore.currentlyRenderingFiber.memoizedState = hookCore.workInProgressHook = newHook;
+        hookCore.firstWorkInProgressHook = hookCore.workInProgressHook = newHook;
     } else {
         // Append to the end of the list.
         hookCore.workInProgressHook = hookCore.workInProgressHook.next = newHook;
@@ -37,7 +38,7 @@ const pushBackHookList = (newHook) => {
  * 만약 wip가 있다면 hookCore의 list에 append합니다.
  */
 export const mountWorkInProgressHook = () => {
-    const hook = createHookObject(null, null, null);
+    const hook = createHookObject(null, null, null, null, null);
     pushBackHookList(hook);
     return hookCore.workInProgressHook;
 };
@@ -51,46 +52,34 @@ export const mountWorkInProgressHook = () => {
  * @returns {import("../types/THookObject.js").THookObject}
  * @description This function updates the work-in-progress hook.
  * 해당 함수는 hookCore의 currentHook과 workInProgressHook를 업데이트하는 것에 중점을 두고 있습니다.
- * 로직의 덩어리는 총 3군데이며,
- *  첫번째 nextCurrentHook을 결정.
- *  두번째 nextWorkInProgressHook을 결정.
- *  세번째 실제로 hookCore의 currentHook과 workInProgressHook을 업데이트.
- *      이 부분에서 이미 wip하고 있는 Hook이 존재한다면, 그것을 재사용하고, 그렇지 않다면 새로운 Hook을 생성하여 사용합니다.
+ * 16.12.0ver 기준으로 firstWorkInProgressHook, nextWorkInProgressHook, nextCurrentHook, currentHook이
+ * 업데이트되었는데 모두 해당 함수 updateWorkInProgressHook에서 관리됩니다.
+ * nextWorkInProgressHook은 말 그대로 다음 workInProgressHook을 가리킵니다.
+ * 처음에는 firstWorkInProgressHook을 넣어 관리합니다. @see renderWithHooks
  */
 export const updateWorkInProgressHook = () => {
-    let nextCurrentHook;
-    if (hookCore.currentHook === null) {
-        const current = hookCore.currentlyRenderingFiber.alternate;
-        if (current !== null) {
-            nextCurrentHook = current.memoizedState;
-        } else {
-            nextCurrentHook = null;
-        }
-    } else {
-        nextCurrentHook = hookCore.currentHook.next;
-    }
-
-    let nextWorkInProgressHook;
-    if (hookCore.workInProgressHook === null) {
-        nextWorkInProgressHook = hookCore.currentlyRenderingFiber.memoizedState;
-    } else {
-        nextWorkInProgressHook = hookCore.workInProgressHook.next;
-    }
-
-    // NOTE: 3번째 이후 update일 때 아래 로직이 불릴 것으로 추정.
-    if (nextWorkInProgressHook !== null) {
+    if (hookCore.nextWorkInProgressHook !== null) {
         // There's already a work-in-progress. Reuse it.
-        hookCore.workInProgressHook = nextWorkInProgressHook;
+        hookCore.workInProgressHook = hookCore.nextWorkInProgressHook;
 
-        nextWorkInProgressHook = hookCore.workInProgressHook.next;
-        hookCore.currentHook = nextCurrentHook;
-        throw Error("updateWorkInProgressHook error!!! OMG");
+        hookCore.nextWorkInProgressHook = hookCore.workInProgressHook.next;
+        hookCore.currentHook = hookCore.nextCurrentHook;
+        hookCore.nextCurrentHook = hookCore.currentHook !== null ? hookCore.currentHook.next : null;
     } else {
-        // Clone the current hook.
-        hookCore.currentHook = nextCurrentHook;
+        // Clone from the current hook.
+        // 여기에 왔다는 것은 현재 해당 fiber에 대한 hookObject가 없다는 것을 의미합니다.
+        // 따라서 currentHook(이전 렌더링 hook)을 기반으로 새로운 hook을 만들어야 합니다.
+        hookCore.currentHook = hookCore.nextCurrentHook;
 
-        const newHook = createHookObject(hookCore.currentHook.memoizedState, hookCore.currentHook.queue, null);
+        const newHook = createHookObject(
+            hookCore.currentHook.memoizedState,
+            hookCore.currentHook.baseState,
+            hookCore.currentHook.baseUpdate,
+            hookCore.currentHook.queue,
+            null
+        );
         pushBackHookList(newHook);
+        hookCore.nextCurrentHook = hookCore.currentHook.next;
     }
     return hookCore.workInProgressHook;
 };
