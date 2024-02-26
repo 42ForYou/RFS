b183d8b9cf8128c4ea45c2db40d180ac4aca4570
JinMu Go <jingmuio@gmail.com>
2024-02-26 10:56:56 +0900

    FY-193/feat(hook): back to the 16.12.0 renderWithHooks

--- srcs/hooks/core/renderWithHooks.js
+++ srcs/hooks/core/renderWithHooks.js
@@ -1,17 +1,35 @@
-import hookCore from "./hookCore";
-import { hookDispatcherOnMount, hookDispatcherOnUpdate } from "./hooksDispatcher";
+import { NoWork } from "../../fiber/fiberExpirationTime.js";
+import hookCore from "./hookCore.js";
+import hookExpirationTime from "./hookExpirationTime.js";
+import hookRenderPhase from "./hookRenderPhase.js";
+import { hookDispatcherOnMount, hookDispatcherOnUpdate } from "./hooksDispatcher.js";
 
 /**
  *
  * @param {Ojbect} hookCore
+ * @param {Object} hookExpirationTime
  * @description This function is used to finish rendering hooks.
  * 하나의 컴포넌트의 렌더링이 끝났기 때문에 hookCore에 존재하던 property들을 초기화합니다.
  * @see hookCore
  */
-const finishRenderingHooks = (hookCore) => {
+const finishRenderingHooks = (hookCore, hookExpirationTime) => {
+    // finish hookCore
     hookCore.currentlyRenderingFiber = null;
-    hookCore.workInProgressHook = null;
+
     hookCore.currentHook = null;
+    hookCore.nextCurrentHook = null;
+
+    hookCore.firstWorkInProgressHook = null;
+    hookCore.workInProgressHook = null;
+    hookCore.nextWorkInProgressHook = null;
+
+    hookCore.componentUpdateQueue = null;
+    hookCore.effectTag = 0;
+
+    // finish hookExpirationTime
+    // TODO: import NoWork
+    hookExpirationTime.renderExpirationTime = NoWork;
+    hookExpirationTime.remainingExpirationTime = NoWork;
 };
 
 /**
@@ -21,22 +39,67 @@ const finishRenderingHooks = (hookCore) => {
  * @param {TFiber} workInProgress
  * @param {Function} Component
  * @param {any} props
- * @param {ref | context} secondArg
+ * @param {ref | context} refOrContext
+ * @param {ExpirationTime} nextRenderExpirationTime
  * @returns {any} children
  */
-export default renderWithHooks = (current, workInProgress, Component, props, secondArg) => {
+export default renderWithHooks = (
+    current,
+    workInProgress,
+    Component,
+    props,
+    refOrContext,
+    nextRenderExpirationTime
+) => {
+    hookExpirationTime.renderExpirationTime = nextRenderExpirationTime;
     hookCore.currentlyRenderingFiber = workInProgress;
+    hookCore.nextCurrentHook = current !== null ? current.memoizedState : null;
 
-    workInProgress.memoizedState = null;
-    workInProgress.updateQueue = null;
-
-    if (current === null || current.memoizedState === null) {
+    if (hookCore.nextCurrentHook === null) {
         hookCore.RfsCurrentDispatcher.current = hookDispatcherOnMount;
     } else {
         hookCore.RfsCurrentDispatcher.current = hookDispatcherOnUpdate;
     }
 
-    const children = Component(props, secondArg);
-    finishRenderingHooks(hookCore);
+    // Component render
+    let children = Component(props, refOrContext);
+
+    // render phase시에 update가 발생했다면 해당 Component를 다시 렌더링합니다.
+    if (hookRenderPhase.didScheduleRenderPhaseUpdate) {
+        do {
+            hookRenderPhase.didScheduleRenderPhaseUpdate = false;
+            hookRenderPhase.numberOfReRenders += 1;
+
+            hookCore.nextCurrentHook = current !== null ? current.memoizedState : null;
+            hookCore.nextWorkInProgressHook = hookCore.firstWorkInProgressHook;
+
+            hookCore.currentHook = null;
+            hookCore.workInProgressHook = null;
+            hookCore.componentUpdateQueue = null;
+
+            hookCore.RfsCurrentDispatcher.current = hookDispatcherOnUpdate;
+
+            children = Component(props, refOrContext);
+        } while (hookRenderPhase.didScheduleRenderPhaseUpdate);
+
+        // reset hookRenderPhase
+        hookRenderPhase.renderPhaseUpdates = null;
+        hookRenderPhase.numberOfReRenders = 0;
+    }
+
+    // 이 과정에서는 hook이 불리면 안되는 상황이기 때문에 (이미 Component가 렌더링이 끝났기 때문에)
+    // ContextOnlyDispatcher를 사용하여 만약 hook이 불렸을 경우 에러를 발생시킵니다.
+    hookCore.RfsCurrentDispatcher.current = ContextOnlyDispatcher;
+
+    const renderedWork = hookCore.currentlyRenderingFiber;
+
+    // 이번 render에서 발생한 결과들을 fiber에 저장합니다.
+    renderedWork.memoizedState = hookCore.firstWorkInProgressHook;
+    renderedWork.expirationTime = hookExpirationTime.renderExpirationTime;
+    renderedWork.updateQueue = hookCore.componentUpdateQueue;
+    renderedWork.effectTag |= hookCore.effectTag;
+
+    // finish rendering hooks
+    finishRenderingHooks(hookCore, hookExpirationTime);
     return children;
 };
