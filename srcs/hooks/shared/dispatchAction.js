5964a0d02201bcf3399f6a3e698b0baaabe72f24
JinMu Go <jingmuio@gmail.com>
2024-02-26 11:01:29 +0900

    FY-193/feat(hook): back to the 16.12.0 useReducer & dispatchAction

---
+++ srcs/hooks/shared/dispatchAction.js
@@ -0,0 +1,93 @@
+import { NoWork } from "../../fiber/fiberExpirationTime.js";
+import is from "../../shared/objectIs.js";
+import createHookUpdate from "../constructor/hookUpdate.js";
+import hookCore from "../core/hookCore.js";
+import hookExpirationTime from "../core/hookExpirationTime.js";
+import hookRenderPhase from "../core/hookRenderPhase.js";
+import enqueueRenderPhaseUpdate from "./enqueueRenderPhaseUpdate.js";
+
+/**
+ * @param {TFiber} fiber currentlyRenderingFiber
+ * @param {THookUpdateQueue} queue
+ * @param {any} action
+ * @description - This function dispatches an action to the reducer.
+ * 다음과 같은 이유로, useReducer에서는 eagerState를 사용하지 않습니다.
+ * useReducer는 보다 복잡한 상태 로직을 관리할 때 사용되며,
+ * 상태 업데이트 로직이 useState보다 더 복잡하거나 조건부 로직을 포함할 수 있기 때문에,
+ * 선제적 계산을 기본 동작으로 사용하지 않습니다.
+ *
+ * 하지만 해당 dispatchReducerAction은 useState와 함께 사용할 예정이기 때문에
+ * eagerState를 추가하여 useState에서 선제적 계산(eager)을 사용하도록 하였습니다.
+ *
+ *  현재 RFS의 schedule logic을 포괄적으로 파악하지 못하였기 때문에
+ *  dispatch로 인해 scheduleUpdateOnFiber를 호출하는 것으로 대체하였습니다.
+ *  해당 65번째 줄의 함수는 이후 구현에 따라 변경될 수 있습니다. react Source에서 render phase일 시에
+ *  enqueueRenderPhaseUpdate를 호출하고 있습니다.
+ */
+const dispatchAction = (fiber, queue, action) => {
+    const alternate = fiber.alternate;
+
+    if (
+        fiber === hookCore.currentlyRenderingFiber ||
+        (alternate !== null && alternate === hookCore.currentlyRenderingFiber)
+    ) {
+        // This is a render phase update.
+
+        hookRenderPhase.didScheduleRenderPhaseUpdate = true;
+
+        const update = createHookUpdate(hookExpirationTime.renderExpirationTime, null, action, null, null, null);
+
+        if (hookRenderPhase.renderPhaseUpdates === null) {
+            hookRenderPhase.renderPhaseUpdates = new Map();
+        }
+
+        const firstRenderPhaseUpdate = hookRenderPhase.renderPhaseUpdates.get(queue);
+        if (firstRenderPhaseUpdate === undefined) {
+            // create new Map for restore update during render phase
+            hookRenderPhase.renderPhaseUpdates.set(queue, update);
+        } else {
+            let lastRenderPhaseUpdate = firstRenderPhaseUpdate;
+            while (lastRenderPhaseUpdate.next !== null) {
+                lastRenderPhaseUpdate = lastRenderPhaseUpdate.next;
+            }
+            lastRenderPhaseUpdate.next = update;
+        }
+    } else {
+        // this is an idle status update
+
+        // TODO: Implement this function. requestCurrentTimeForUpdate
+        const currentTime = requestCurrentTimeForUpdate();
+        // TODO: Implement this function. requestCurrentSuspenseConfig
+        const suspenseConfig = requestCurrentSuspenseConfig();
+        // TODO: Implement this function. computeExpirationForFiber
+        const expirationTime = computeExpirationForFiber(currentTime, fiber, suspenseConfig);
+
+        const update = createHookUpdate(expirationTime, suspenseConfig, action, null, null, null);
+        enqueueRenderPhaseUpdate(queue, update);
+
+        if (fiber.expirationTime === NoWork && (alternate === null || alternate.expirationTime === NoWork)) {
+            // The queue is currently empty, which means we can eagerly compute the
+            // next state before entering the render phase. If the new state is the
+            // same as the current state, we may be able to bail out entirely.
+            const lastRenderedReducer = queue.lastRenderedReducer;
+            if (lastRenderedReducer !== null) {
+                try {
+                    const currentState = queue.lastRenderedState;
+                    const eagerState = lastRenderedReducer(currentState, action);
+
+                    update.eagerReducer = lastRenderedReducer;
+                    update.eagerState = eagerState;
+                    if (is(eagerState, currentState)) {
+                        return;
+                    }
+                } catch (error) {
+                    // Suppress the error. It will throw again in the render phase.
+                }
+            }
+        }
+        // TODO: Implement this function. scheduleWork
+        scheduleWork(fiber, expirationTime);
+    }
+};
+
+export default dispatchAction;
--- srcs/hooks/useReducer/useReducerImpl.js
+++ srcs/hooks/useReducer/useReducerImpl.js
@@ -5,77 +5,147 @@
 
 import is from "../../shared/objectIs.js";
 import hookCore from "../core/hookCore.js";
+import hookRenderPhase from "../core/hookRenderPhase.js";
 import { mountWorkInProgressHook, updateWorkInProgressHook } from "../core/workInProgressHook.js";
-
-import { createHookUpdate, createUpdateQueue } from "../constructor/index.js";
-import enqueueRenderPhaseUpdate from "../shared/enqueueRenderPhaseUpdate.js";
-
-/**
- * @param {TFiber} fiber currentlyRenderingFiber
- * @param {THookUpdateQueue} queue
- * @param {any} action
- * @description - This function dispatches an action to the reducer.
- * 다음과 같은 이유로, useReducer에서는 eagerState를 사용하지 않습니다.
- * useReducer는 보다 복잡한 상태 로직을 관리할 때 사용되며,
- * 상태 업데이트 로직이 useState보다 더 복잡하거나 조건부 로직을 포함할 수 있기 때문에,
- * 선제적 계산을 기본 동작으로 사용하지 않습니다.
- *
- * 하지만 해당 dispatchReducerAction은 useState와 함께 사용할 예정이기 때문에
- * eagerState를 추가하여 useState에서 선제적 계산(eager)을 사용하도록 하였습니다.
- *
- *  현재 RFS의 schedule logic을 포괄적으로 파악하지 못하였기 때문에
- *  dispatch로 인해 scheduleUpdateOnFiber를 호출하는 것으로 대체하였습니다.
- *  해당 65번째 줄의 함수는 이후 구현에 따라 변경될 수 있습니다. react Source에서 render phase일 시에
- *  enqueueRenderPhaseUpdate를 호출하고 있습니다.
- */
-const dispatchReducerAction = (fiber, queue, action) => {
-    const update = createHookUpdate(action, false, null, null);
-    enqueueRenderPhaseUpdate(queue, update);
-    const lastRenderedReducer = queue.lastRenderedReducer;
-    const currentState = queue.lastRenderedState;
-    const eagerState = lastRenderedReducer(currentState, action);
-
-    if (is(eagerState, currentState)) {
-        return;
-    }
-
-    // TODO: Implement this function.
-    scheduleUpdateOnFiber(fiber);
-};
+import { createHookUpdateQueue } from "../constructor/index.js";
+import hookExpirationTime from "../core/hookExpirationTime.js";
+import dispatchAction from "../shared/dispatchAction.js";
 
 /**
  * @param {THookObject} hook
- * @param {THookObject} currentHook
  * @param {Function} reducer
  * @returns {[any, Function]} - [state, dispatch]
  *
  * @description - This function updates the reducer.
- * updateReducer는 rendering 시에 호출되며, 한번 mount된 Component에서 다시 호출되었을 때
- * 사용됩니다. 해당 함수를 useState와 함께 쓸 것을 고려하여 hasEagerState분기를 넣었습니다.
- * hasEagerState가 true일 경우, eagerState를 사용하여 state를 업데이트합니다.
+ * updateReducer는 redner phase혹은 idle status일 때 호출되며
+ * 한번 mount된 Component에서 다시 호출되었을 때 사용됩니다.
+ * 전체적인 로직은
+ *  1. render phase update가 있는지 확인합니다.
+ *  2. idle status update가 있는지 확인합니다.
+ *
+ * 해당 함수를 useState와 함께 쓸 것을 고려하여 eagerState분기를 넣었습니다.
+ * eagerReducer가 reducer와 같을 경우, eagerState를 사용하여 state를 업데이트합니다.
  */
-const updateReducerImpl = (hook, currentHook, reducer) => {
+const updateReducerImpl = (hook, reducer) => {
     const queue = hook.queue;
     queue.lastRenderedReducer = reducer;
-    const lastRenderPhaseUpdate = queue.pending;
-    let newState = currentHook.memoizedState;
 
-    if (lastRenderPhaseUpdate !== null) {
-        queue.pending = null;
-        const first = lastRenderPhaseUpdate.next;
-        let update = first;
-        do {
-            const action = update.action;
-            if (update.hasEagerState) {
-                // If this update is a state update (not a reducer) and was processed eagerly,
-                // we can use the eagerly computed state
-                newState = update.eagerState;
-            } else {
-                newState = reducer(newState, action);
+    // render-phase update (1번째 분기)
+    if (hookRenderPhase.numberOfReRenders > 0) {
+        // render-phase update
+        // This is a re-render. Apply the new reduer phase updates to the previous
+        // work-in-progress hook.
+
+        const dispatch = queue.dispatch;
+
+        // 만약 render-phase update가 있다면, 해당 update를 적용해야한다.
+        // renderPhaseUpdates는 Map으로 구성되어있으며, queue를 key로 가지고 있다.
+        // 이를 linked list로 변환하여 순회한다.
+        if (hookRenderPhase.renderPhaseUpdates !== null) {
+            const firstRenderPhaseUpdate = hookRenderPhase.renderPhaseUpdates.get(queue);
+            if (firstRenderPhaseUpdate !== undefined) {
+                hookRenderPhase.renderPhaseUpdates.delete(queue);
+                let newState = hook.memoizedState;
+                let update = firstRenderPhaseUpdate;
+                do {
+                    const action = update.action;
+                    newState = reducer(newState, action);
+                    update = update.next;
+                } while (update !== null);
+
+                if (!is(newState, hook.memoizedState)) {
+                    // TODO: Implement this function.
+                    // 함수 구현이 간단하긴 하지만 beginWork의 module scope를 참조해야 하기 때문에
+                    // 이후 추가 구현을 위해 주석처리 하였습니다
+                    markWorkInProgressReceivedUpdate();
+                }
+
+                hook.memoizedState = newState;
+
+                if (hook.baseState === queue.last) {
+                    hook.baseState = newState;
+                }
+
+                queue.lastRenderedState = newState;
+
+                return [newState, dispatch];
             }
+        }
+        return [hook.memoizedState, dispatch];
+    }
+
+    // idle status update
+    const last = queue.last;
+
+    const baseUpdate = hook.baseUpdate;
+    const baseState = hook.baseState;
+
+    // check the update during the idle status
+    let first;
+    if (baseUpdate !== null) {
+        if (last !== null) {
+            // circular linked list 더는 head를 물고 있을 필요가 없으므로 연결을 끊어준다.
+            last.next = null;
+        }
+        first = baseUpdate.next;
+    } else {
+        // last.next는 null이 아니라면 first를 가리킨다.
+        first = last !== null ? last.next : null;
+    }
+
+    // (2번째 분기)
+    if (first !== null) {
+        let newState = baseState;
+        let newBaseState = null;
+        let newBaseUpdate = null;
+        let prevUpdate = baseUpdate;
+        let update = first;
+        let didSkip = false;
+        do {
+            const updateExpirationTime = update.expirationTime;
+            // expirationTime을 대소로 비교하는 조건문이 있을 때는 큰 숫자가 더 먼저 발생한 작업
+            if (updateExpirationTime < hookExpirationTime.renderExpirationTime) {
+                if (!didSkip) {
+                    didSkip = true;
+                    newBaseUpdate = prevUpdate;
+                    newBaseState = newState;
+                }
+
+                if (updateExpirationTime > hookExpirationTime.remainingExpirationTime) {
+                    hookExpirationTime.remainingExpirationTime = updateExpirationTime;
+                    // TODO: Implement this function.
+                    markUnprocessedUpdateTime(hookExpirationTime.remainingExpirationTime);
+                }
+            } else {
+                // This update does have sufficient priority.
+                // TODO: implement this function.
+                markRenderEventTimeAndConfig(updateExpirationTime, update.suspenseConfig);
+
+                if (update.eagerReducer === reducer) {
+                    newState = update.eagerState;
+                } else {
+                    const action = update.action;
+                    newState = reducer(newState, action);
+                }
+            }
+            prevUpdate = update;
             update = update.next;
-        } while (update !== first);
+        } while (update !== null && update !== first);
+
+        if (!didSkip) {
+            newBaseUpdate = prevUpdate;
+            newBaseState = newState;
+        }
+
+        if (!is(newState, hook.memoizedState)) {
+            // TODO: Implement this function.
+            markWorkInProgressReceivedUpdate();
+        }
+
         hook.memoizedState = newState;
+        hook.baseUpdate = newBaseUpdate;
+        hook.baseState = newBaseState;
+
         queue.lastRenderedState = newState;
     }
 
@@ -92,12 +162,12 @@ const updateReducerImpl = (hook, currentHook, reducer) => {
  * @description - This function update the reducer.
  * updateWorkInProgressHook과 updateReducerImpl을 사용하여
  * Component가 업데이트될 때 사용됩니다.
- *  1. props나 상태가 변경되어 재렌더링이 필요할 때.
- *  2. 액션이 dispatch될 때 (dispatch를 통해 상태를 변경할 때)
+ *  1. props나 상태가 변경되어 재렌더링이 필요할 때. (render phase update)
+ *  2. 액션이 dispatch될 때 (dispatch를 통해 상태를 변경할 때) (idle status update)
  */
 export const updateReducer = (reducer, _, __) => {
     const hook = updateWorkInProgressHook();
-    return updateReducerImpl(hook, hookCore.currentHook, reducer);
+    return updateReducerImpl(hook, reducer);
 };
 
 /**
@@ -119,8 +189,8 @@ export const mountReducer = (reducer, initialArg, init) => {
     } else {
         initialState = initialArg;
     }
-    hook.memoizedState = initialState;
-    const queue = createUpdateQueue(null, null, reducer, initialState);
+    hook.memoizedState = hook.baseState = initialState;
+    const queue = createHookUpdateQueue(null, null, reducer, initialState);
     hook.queue = queue;
 
     // dispatchReducerAction함수는 이후 사용자가 dispatch를 호출할 때마다 호출되는 내부 구현입니다.
@@ -128,7 +198,7 @@ export const mountReducer = (reducer, initialArg, init) => {
     // 인자를 fiber, queue, action으로 받습니다
     // 하지만 사용자는 내부 변수인 fiber와 queue를 알 필요가 없기 때문에,
     // 해당 함수는 호출할 때는 Action만 인자로 넣도록 dispatch를 bind를 이용해 생성합니다.
-    const dispatch = dispatchReducerAction.bind(null, hookCore.currentlyRenderingFiber, queue);
+    const dispatch = dispatchAction.bind(null, hookCore.currentlyRenderingFiber, queue);
     queue.dispatch = dispatch;
     return [hook.memoizedState, dispatch];
 };
