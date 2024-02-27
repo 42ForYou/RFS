3587e3830458fc50f998d78cdf7f4649a87343b9
JinMu Go <jingmuio@gmail.com>
2024-02-27 13:35:50 +0900

    FY-193/feat(hook): renew useLayoutEffect

--- srcs/hooks/useLayoutEffect/useLayoutEffectImpl.js
+++ srcs/hooks/useLayoutEffect/useLayoutEffectImpl.js
@@ -1,6 +1,6 @@
-import { LayoutStatic as LayoutStaticEffect, Update as UpdateEffect } from "../../fiber/type.js";
+import { Update as UpdateEffect } from "../../fiber/type.js";
 
-import { Layout as HookLayout } from "../types/THookEffectFlags.js";
+import { MountLayout, UnmountMutation } from "../types/THookEffectFlags.js";
 import { mountEffectImpl, updateEffectImpl } from "../useEffect/useEffectImpl.js";
 
 /**
@@ -12,11 +12,13 @@ import { mountEffectImpl, updateEffectImpl } from "../useEffect/useEffectImpl.js
  * 하지만 아직 Static에 대한 것은 알지 못합니다.
  * Update Flag는 useEffect를 제외한 다른 Effect훅들은 모두 Fiber에 UpdateEffect를 사용합니다.
  * 정확한 이유는 아직 모르겠습니다.
+ * // NOTE: 16.12.0 version에서는 Static Flag가 사라졌습니다.
+ * // 다른 플래그에 대한 조사를 Notion에 적어 놓겠습니다.
  * @see mountEffectImpl
  * @returns
  */
 export const mountLayoutEffect = (create, deps) => {
-    return mountEffectImpl(UpdateEffect | LayoutStaticEffect, HookLayout, create, deps);
+    return mountEffectImpl(UpdateEffect, UnmountMutation | MountLayout, create, deps);
 };
 
 /**
@@ -29,5 +31,5 @@ export const mountLayoutEffect = (create, deps) => {
  * @returns
  */
 export const updateLayoutEffect = (create, deps) => {
-    return updateEffectImpl(UpdateEffect, HookLayout, create, deps);
+    return updateEffectImpl(UpdateEffect, UnmountMutation | MountLayout, create, deps);
 };
