c77f1487b553ef1d722a8d9b544958fad2812133
daekuelee <daekuelee@gmail.com>
2024-02-29 04:22:19 +0900

    FY-199/refactor(fiber): TFiber의 설명을 보다 정확히수정
    해당 내용의 자세한 설명은 모두 한국말로 변경하였습니다. 그리고 해당 property에 대한 보다 자세한
    내용을 서술하였고 결과물이 어느정도 나온뒤 명세를 정확히 할 예정입니다.->버전패치된 것에 대한

--- srcs/fiber/type/TWorkTag.js
+++ srcs/fiber/type/TWorkTag.js
@@ -2,11 +2,14 @@
 /**
  *@typedef TWorkTag
  */
-export const TWorkTag = Number; // only for editor to recognize the type
-export const FunctionComponent = 0; //to represent the function component
-export const HostRoot = 1; // to represent the HostTree Root Node
-export const HostComponent = 2; // to represent the Host Component
-export const HostText = 3; // to represent the Host Text
-export const Fragment = 4; // to represent the Fragment
-export const ContextProvider = 5; // to represent the Context Provider
-export const MemoComponent = 6; // to represent the HighOrderComponent created by memo
+export const TWorkTag = Number; // 파이버의 작동방식을 결정하는 타입으로 숫자형으로 정의한다.
+export const FunctionComponent = 0; //함수형 컴포넌트를 위한 Fiber
+// export const ClassComponent = X; //클래스형 컴포넌트를 위한 Fiber-현 리액트에선 구현하지 않는다
+export const HostRoot = 1; // 호스트의 루트를 위한 Fiber-> 이 부분에서는 따로 처리해야되는 부분이 많다.
+export const HostComponent = 2; // 실제 돔에 표현되는 컴포넌트를 위한 Fiber
+export const HostText = 3; // 호스트 Text->즉 돔에 텍스트를 표현하기 위한 Fiber
+export const Fragment = 4; // 단순이 컴포넌트를 그룹핑하기위한 Fiber-> div그루핑을 막는다
+export const ContextProvider = 5; // 컨텍스트를 제공하는 Fiber
+// export const MemoComponent = X; // 본 리액트에서는 해당 타입을 통해서 함수형과 클래스형 메모 컴포넌트를 구분하지만
+// 우리의 리액트는 함수형 컴포넌트만을 지원하므로 해당 타입은 필요하지 않다.
+export const SimpleMemoComponent = 6; // 함수형 컴포넌트를 위한 메모 컴포넌트
