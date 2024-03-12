1f27c508d203b2e84f359b60e1a9851907a1c5fb
daekuelee <daekuelee@gmail.com>
2024-03-13 06:37:38 +0900

    FY-213/feat(work): beginWork의 update부분 구현
    해당 함수의 update관련된 함수들을 구현하였습니다.
    여기서 hostPushContext와 같은 context기반 관련 문맥은
    해당 문맥을 해결한훈 주석과 함께 세부로 더 구현하겠습니다.
    또한 재조정 관련된 부분도 reconciler의 추가 부분에서 구현을 진행하겠습니다.
    다음 TODO:로는 processUpdateQueue와, contexthostConfig, reconciler가 있습니다.

---
+++ srcs/shared/sharedEqual.js
@@ -0,0 +1,37 @@
+import is from "objectIs.js";
+
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+
+/**
+ *
+ * @param {any} objA
+ * @param {any} objB
+ * @returns {boolean}
+ * @description  객체의 키를 반복하고 거짓을 반환하여 같음을 수행합니다.
+ * @description 인자 간에 값이 엄격하게 같지 않은 키가 있을 때 반환합니다.
+ * @description 모든 키의 값이 엄격하게 같으면 참을 반환합니다.
+ */
+export const shallowEqual = (objA, objB) => {
+    if (is(objA, objB)) {
+        return true;
+    }
+
+    if (typeof objA !== "object" || objA === null || typeof objB !== "object" || objB === null) {
+        return false;
+    }
+
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+        return false;
+    }
+
+    for (let i = 0; i < keysA.length; i++) {
+        if (!hasOwnProperty.call(objB, keysA[i]) || !is(objA[keysA[i]], objB[keysA[i]])) {
+            return false;
+        }
+    }
+
+    return true;
+};
--- srcs/work/beginWork.js
+++ srcs/work/beginWork.js
@@ -12,7 +12,226 @@ import {
     ContextProvider,
     SimpleMemoComponent,
 } from "../const/CWorkTag.js";
+import { Placement, PerformedWork } from "../const/CSideEffectFlags.js";
+import { Update as UpdateEffect, Passive as PassiveEffect } from "../const/CSideEffectFlags.js";
 import { cloneChildFibers } from "../fiber/childFiber.js";
+import { renderWithHooks } from "../hooks/core/renderWithHooks.js";
+import { push } from "../fiber/fiberStack.js";
+import { shallowEqual } from "../shared/sharedEqual.js";
+
+/**
+ *
+ * @param {TFiber|null} current
+ * @param {TFiber} workInProgress
+ * @param {any} nextChildren
+ * @param {TExpirationTime} renderExpirationTime
+ * @description 해당함수는 현재 파이버의 자식을 재조정하는 함수입니다.
+ */
+export const reconcileChildren = (current, workInProgress, nextChildren, renderExpirationTime) => {
+    if (current === null) {
+        //TODO: mountChildFibers 구현
+        // 아직 렌더링되지 않은 새 컴포넌트인 경우, 우리는
+        // 최소한의 sideEffect을 적용하여 자식 집합을 업데이트하지 않습니다. 대신
+        // 렌더링되기 전에 자식에 모두 추가합니다(모든 사이드 이펙트를 가함). 즉
+        // 부작용을 추적하지 않음으로써 이 조정 패스를 최적화할 수 있습니다.
+        workInProgress.child = mountChildFibers(workInProgress, null, nextChildren, renderExpirationTime);
+    } else {
+        //TODO: reconcileChildFibers 구현
+        // 현재 자식이 진행 중인 작업과 동일하면 다음을 의미합니다.
+        // 아직 이 자식에 대한 작업을 시작하지 않았다는 뜻입니다. 따라서 우리는
+        // 복제 알고리즘을 사용하여 현재 모든 자식의 복사본을 만듭니다.
+
+        // 이미 진행 중인 작업이 있다면 이 시점에서는 유효하지 않으므로
+        // 버리겠습니다.
+        workInProgress.child = reconcileChildFibers(workInProgress, current.child, nextChildren, renderExpirationTime);
+    }
+};
+/**
+ * TODO: 해당 함수의 문맥은 processUpdateQueue를 정확히 이해해야됨
+ * @param {TFiber} current
+ * @param {TFiber} workInProgress
+ * @param {TFiber} renderExpirationTime
+ * @returns {TFiber|null}
+ * @description HostRootComponent를 업데이트하는 함수
+ */
+const updateHostRoot = (current, workInProgress, renderExpirationTime) => {
+    //TODO: pushHostRootContext 문맥이해
+    pushHostRootContext(workInProgress);
+    const updateQueue = workInProgress.updateQueue;
+    const nextProps = workInProgress.pendingProps;
+    //TODO: memoizedState가 어떤식으로 들어가는지 확인
+    const prevState = workInProgress.memoizedState;
+    const prevChildren = prevState !== null ? prevState.element : null;
+
+    //updateQueue를 처리한다
+    //TODO: processUpdateQueue 구현
+    processUpdateQueue(workInProgress, updateQueue, nextProps, null, renderExpirationTime);
+    //updateQueue를 처리한 후에는 memoizedState가 바뀌었을 수 있으므로 다시 가져온다.
+    const nextState = workInProgress.memoizedState;
+    const nextChildren = nextState.element;
+    //이전 자식과 같으면 bailout
+    if (nextChildren === prevChildren) {
+        return bailoutOnAlreadyFinishedWork(current, workInProgress, renderExpirationTime);
+    }
+    const root = workInProgress.stateNode;
+    //TODO: reconcileChildren 구현
+    reconcileChildren(current, workInProgress, nextChildren, renderExpirationTime);
+    return workInProgress.child;
+};
+
+/**
+ *
+ * @param {TFiber} current
+ * @param {TFiber} workInProgress
+ * @description ref가 교체 되었으면 effectTag에 Ref를 업데이트 해야된다라고 마킹하는 함수
+ */
+const markRef = (current, workInProgress) => {
+    const ref = workInProgress.ref;
+    if ((current === null && ref !== null) || (current !== null && current.ref !== ref)) {
+        workInProgress.effectTag |= Ref;
+    }
+};
+
+/**
+ *
+ * @param {TFiber} current
+ * @param {TFiber} workInProgress
+ * @param {TExpirationTime} renderExpirationTime
+ * @returns {TFiber | null}
+ * @description HostComponent를 업데이트하는 함수 호스트 컴포넌트는 문자열하나만 자식으로 가지고 있을떄 특수처리합니다.
+ */
+const updateHostComponent = (current, workInProgress, renderExpirationTime) => {
+    //TODO: pushHostContext 문맥이해
+    pushHostContext(workInProgress);
+
+    const type = workInProgress.type;
+    const nextProps = workInProgress.pendingProps;
+    const prevProps = current !== null ? current.memoizedProps : null;
+
+    let nextChildren = nextProps.children;
+    //TODO: shouldSetTextContent 구현 DOM모듈에서
+    //호스트 컴포넌트는 자식으로 문자열 하나만 가지고 있을떄 해당 문자열을 파이버로 만들지 않음.
+    const isDirectTextChild = shouldSetTextContent(type, nextProps);
+    if (isDirectTextChild) {
+        //이것은 문자열을 파이버로 만들지 않기 위해 초기화합니다.
+        // 호스트 노드의 직접 텍스트 자식을 특수 처리합니다. 이것은 일반적인
+        // 케이스입니다. 재정의된 자식으로 처리하지 않습니다. 대신 이 프로퍼티에 액세스할 수 있는 호스트 환경에서
+        // 이 프로퍼티에 액세스할 수 있는 호스트 환경에서 처리합니다. 그
+        // 다른 HostText 파이버를 할당하고 순회하는 것을 방지합니다.
+        nextChildren = null;
+    } else if (prevProps !== null && shouldSetTextContent(type, prevProps)) {
+        // 직접 텍스트 자식에서 일반 자식으로 전환하는 경우 또는
+        // 비어있는 경우 텍스트 콘텐츠가 재설정되도록 예약해야 합니다.
+
+        //여기서 문자열이 아닌 다음 자식을 삽입하기전에 current가 문자열만 가졌다면
+        //제거해주고 삭제해야됨
+        //그러나 fiber로 따로 만들지 않았기 떄문에 삭제 로직을 타지 않는데
+        //이를 따로 처리해주기 위해 ContentReset을 마킹해준다.
+        workInProgress.effectTag |= ContentReset;
+    }
+
+    //hostComponent가 업데이트 되면 ref도 업데이트되기 떄문에 마킹
+    markRef(current, workInProgress);
+
+    //TODO: check OffScreen 구현
+
+    //TODO: reconcileChildren 구현
+    reconcileChildren(current, workInProgress, nextChildren, renderExpirationTime);
+    return workInProgress.child;
+};
+/**
+ *
+ * @param {TFiber | null} _current @see 파일경로: /type/TFiber.js
+ * @param {TFiber} workInProgress @see 파일경로: /type/TFiber.js
+ * @param {lambda} Component
+ * @param {TExpirationTime} renderExpirationTime @see 파일경로: /type/TExpirationTime.js
+ * @returns {TFiber|null} @see 파일경로: /type/TFiber.js
+ * @description function Component를 마운트하는 함수
+ */
+const mountIndeterminateComponent = (_current, workInProgress, Component, renderExpirationTime) => {
+    if (_current !== null) {
+        //TODO: 오직 susPense가 일어난 상황에서만 일어나는 로직 - 빼야되는지 확인
+        // An indeterminate component only mounts if it suspended inside a non-
+        // concurrent tree, in an inconsistent state. We want to treat it like
+        // a new mount, even though an empty version of it already committed.
+        // Disconnect the alternate pointers.
+        _current.alternate = null;
+        workInProgress.alternate = null;
+        // Since this is conceptually a new fiber, schedule a Placement effect
+        workInProgress.effectTag |= Placement;
+    }
+
+    const props = workInProgress.pendingProps;
+
+    //TODO: 이 ReadContext는 어떤 역할을 하는지 확인
+    prepareToReadContext(workInProgress, renderExpirationTime);
+    const value = renderWithHooks(null, workInProgress, Component, props, null, renderExpirationTime);
+
+    workInProgress.effectTag |= PerformedWork;
+
+    if (
+        typeof value === "object" &&
+        value !== null &&
+        typeof value.render === "function" &&
+        value.$$typeof === undefined
+    ) {
+        console.error("not supported ClassComponent in mountIndeterminateComponent");
+        throw new Error("not supported ClassComponent in mountIndeterminateComponent");
+    } else {
+        workInProgress.tag = FunctionComponent;
+        //TODO: reconcileChildren 구현
+        reconcileChildren(null, workInProgress, value, renderExpirationTime);
+        return workInProgress.child;
+    }
+};
+
+/**
+ *
+ * @param {TFiber} current
+ * @param {TFiber} workInProgress
+ * @param {TExpirationTime} expirationTime
+ * @description update를 하게되면 component를 실행시키게 되는데 업데이트가 안되면 그 전 관련
+ * @description 훅작업을 취소 시켜야됨 그것의 역할을 하는 함수
+ */
+const bailoutHooks = (current, workInProgress, expirationTime) => {
+    //훅의 잔여물을 제거하는 역할
+    workInProgress.updateQueue = current.updateQueue;
+    //passiveEffect랑 updateEffect를 제거하는 역할
+    workInProgress.effectTag &= ~(PassiveEffect | UpdateEffect);
+    //현재 expirationTime보다 이후것 생겼으면 잔여물->제거
+    if (current.expirationTime <= expirationTime) {
+        current.expirationTime = NoWork;
+    }
+};
+
+/**
+ * @param {TFiber | null} _current
+ * @param {TFiber} workInProgress
+ * @param {lambda} Component
+ * @param {any} nextProps
+ * @param {TExpirationTime} renderExpirationTime
+ * @returns {TFiber|null}
+ * @description function Component를 업데이트하는 함수
+ */
+const updateFunctionComponent = (_current, workInProgress, Component, nextProps, renderExpirationTime) => {
+    prepareToReadContext(workInProgress, renderExpirationTime);
+    const nextChildren = renderWithHooks(_current, workInProgress, Component, nextProps, null, renderExpirationTime);
+
+    //     beginWork()에서 확인한 결과 props는 변경되지 않았다.
+    // 하지만 업데이트가 발생한 컴포넌트이므로 호출되어야 한다.
+    // 컴포넌트 호출 후에도 didReceiveUpdate는 여전히 false임을 미루어보아 컴포넌트 상태 또한 변경되지 않았다.
+    // props, state 모두 변경되지 않았다면 서브 트리 또한 변경될 부분이 없으므로 Work를 여기서 끊어주게 될 경우 불필요한 작업을 하지 않아도 된다.
+    // 문제는 컴포넌트가 한번은 호출되었기 때문에 라이프 사이클 훅(useEffect(), useLayoutEffect())의 잔여물이 fiber에 남아있다.
+    // 잔여물을 제거하고 bailoutOnAlreadyFinishedWork()를 진행한다. 만약 자손에서 업데이트 컴포넌트가 있다면 자식 workInProgress가 반환되어 계속해서 밑으로 Work가 진행될 것이지만, 없다면 여기서 Work는 종료된다.
+    if (_current !== null && !didReceiveUpdate) {
+        bailoutHooks(_current, workInProgress, renderExpirationTime);
+        return bailoutOnAlreadyFinishedWork(_current, workInProgress, renderExpirationTime);
+    }
+
+    workInProgress.effectTag |= PerformedWork;
+    reconcileChildren(_current, workInProgress, nextChildren, renderExpirationTime);
+    return workInProgress.child;
+};
 
 /**
  *
@@ -143,6 +362,38 @@ const updateContextConsumer = (current, workInProgress, renderExpirationTime) =>
     return workInProgress.child;
 };
 
+/**
+ *
+ * @param {TFiber} current
+ * @param {TFiber} workInProgress
+ * @param {lambda} Component
+ * @param {any} nextProps
+ * @param {TExpirationTime} updateExpirationTime
+ * @param {TExpirationTime} renderExpirationTime
+ * @returns {TFiber}
+ * @description - updateSimpleMemoComponent의 목표는 memo를 사용하여 최적화된 컴포넌트를 업데이트하는 것입니다.
+ */
+const updateSimpleMemoComponent = (
+    current,
+    workInProgress,
+    Component,
+    nextProps,
+    updateExpirationTime,
+    renderExpirationTime
+) => {
+    if (current !== null) {
+        const prevProps = current.memoizedProps;
+        if (shallowEqual(prevProps, nextProps) && current.ref === workInProgress.ref) {
+            //update를 취소해야 함
+            didReceiveUpdate = false;
+            if (updateExpirationTime < renderExpirationTime) {
+                // This will bail out and read the memoized state.
+                return bailoutOnAlreadyFinishedWork(current, workInProgress, renderExpirationTime);
+            }
+        }
+    }
+    return updateFunctionComponent(current, workInProgress, Component, nextProps, renderExpirationTime);
+};
 //TODO: 명세랑 정확한 동작 정의
 const pushHostRootContext = (workInProgress) => {
     //FiberRoot
@@ -150,6 +401,31 @@ const pushHostRootContext = (workInProgress) => {
     //TODO:: fiberStack이랑 연계되어야될듯
     //TODO: pushHostContainer 구현
     pushHostContainer(workInProgress, root.containerInfo);
+};
+
+/**
+ *
+ * @param {TFiber} current
+ * @param {TFiber} workInProgress
+ * @returns {null}
+ * @description 여기선 할일이 없습니다. 뒤의 Completionphase에서 처리합니다.
+ */
+const updateHostText = (current, workInProgress) => {
+    return null;
+};
+
+/**
+ *
+ * @param {TFiber} current
+ * @param {TFiber} workInProgress
+ * @param {TExpirationTime} renderExpirationTime
+ * @returns {TFiber|null}
+ * @description Fragment를 업데이트하는 함수 단순히 reconcileChildren을 호출합니다.(단순 래퍼)
+ */
+const updateFragment = (current, workInProgress, renderExpirationTime) => {
+    const nextChildren = workInProgress.pendingProps;
+    reconcileChildren(current, workInProgress, nextChildren, renderExpirationTime);
+    return workInProgress.child;
 };
 
 /**
