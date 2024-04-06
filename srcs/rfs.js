fcfcf8693bbbf6d3eeea3d539ca5b51674c31cc2
daekuelee <daekuelee@gmail.com>
2024-04-06 11:34:54 +0900

    FY-227/feat(root): rfsExport파일 생성
    해당 파일 모듈들을 모아줄 하나의 Rfs객체를 Export할 파일 생성하였습니다.

---
+++ srcs/rfs.js
@@ -0,0 +1,33 @@
+import { RFS_FRAGMENT_TYPE } from "./core/rfsSymbol.js";
+import { createElement } from "./core/rfsElement.js";
+import { createContext } from "./context/createContext.js";
+import forwardRef from "./core/forwardRef.js";
+import memo from "./core/memo.js";
+import useCallback from "./hooks/useCallback/useCallback.js";
+import useContext from "./hooks/useContext/useContext.js";
+import useEffect from "./hooks/useEffect/useEffect.js";
+import useLayoutEffect from "./hooks/useLayoutEffect/useLayoutEffect.js";
+import useMemo from "./hooks/useMemo/useMemo.js";
+import useReducer from "./hooks/useReducer/useReducer.js";
+import useRef from "./hooks/useRef/useRef.js";
+import useState from "./hooks/useState/useState.js";
+const Rfs = {
+    createContext,
+    forwardRef,
+    memo,
+
+    useCallback,
+    useContext,
+    useEffect,
+    useLayoutEffect,
+    useMemo,
+    useReducer,
+    useRef,
+    useState,
+
+    Fragment: RFS_FRAGMENT_TYPE,
+
+    createElement,
+};
+
+export default Rfs;
