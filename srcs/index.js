6eb5199cbaa32814b3586718b18c398ccec56ecb
daekuelee <98301444+daekuelee@users.noreply.github.com>
2024-04-06 11:44:56 +0900

    FY-227/feat(root): rfsExport파일 생성 (#45)
    * FY-227/feat(root): rfsExport파일 생성
    
    해당 파일 모듈들을 모아줄 하나의 Rfs객체를 Export할 파일 생성하였습니다.
    
    * FY-227/fix(root): rfs.js->index.js
    
    file이름 변경
    
    ---------
    
    Co-authored-by: daekuelee <daekuelee@gmail.com>

--- srcs/index.js
+++ srcs/index.js
@@ -1,3 +1,33 @@
-export { createElement } from "./core/rfsElement.js";
+import { RFS_FRAGMENT_TYPE } from "./core/rfsSymbol.js";
+import { createElement } from "./core/rfsElement.js";
+import { createContext } from "./context/createContext.js";
+import forwardRef from "./core/forwardRef.js";
+import memo from "./core/memo.js";
+import useCallback from "./hooks/useCallback/useCallback.js";
+import useContext from "./hooks/useContext/useContext.js";
+import useEffect from "./hooks/useEffect/useEffect.js";
+import useLayoutEffect from "./hooks/useLayoutEffect/useLayoutEffect.js";
+import useMemo from "./hooks/useMemo/useMemo.js";
+import useReducer from "./hooks/useReducer/useReducer.js";
+import useRef from "./hooks/useRef/useRef.js";
+import useState from "./hooks/useState/useState.js";
+const Rfs = {
+    createContext,
+    forwardRef,
+    memo,
 
-export { useReducer, useEffect, useCallback, useRef, useState, useLayoutEffect, useContext } from "./hooks/index.js";
+    useCallback,
+    useContext,
+    useEffect,
+    useLayoutEffect,
+    useMemo,
+    useReducer,
+    useRef,
+    useState,
+
+    Fragment: RFS_FRAGMENT_TYPE,
+
+    createElement,
+};
+
+export default Rfs;
